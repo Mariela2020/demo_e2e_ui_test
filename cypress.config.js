50f813b7449506526b70f9998e1f4fc40992ceb0
Mariela2020 <67210822+Mariela2020@users.noreply.github.com>
2022-08-24 18:54:51 -0400

    Agregando scripts

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ module.exports = {
   viewportWidth: 600,
   viewportHeight: 800,
   defaultCommandTimeout: 4000,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:5000',
   },
+  
 }
 
