b33387d0e14fe7fef87ee2243484f2722f3b4db2
Mariela2020 <67210822+Mariela2020@users.noreply.github.com>
2022-08-24 18:35:07 -0400

    Agrecando TC

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  viewportWidth: 600,
+  viewportHeight: 800,
+  defaultCommandTimeout: 4000,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:5000',
+  },
+}
+
